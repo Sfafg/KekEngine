74e3ae053b2c0f87bdf633cea7b3978ee08e359e
Sławek Zdunek <slawekzdunek2.3.6@gmail.com>
2022-09-14 21:13:49 +0200

    List

--- KekEngine/Source.cpp
+++ KekEngine/Source.cpp
@@ -3,6 +3,11 @@
 #include "Logging/Log.h"
 #include "Maths/Transform.h"
 
+// Fix Column Major Matrices
+// Check if Transform is Done
+// Camera Class
+// Rendering
+
 int main()
 {
 	Transform2D tr({ 10,20 }, { 4,1 });
