99459f203b7ba5672b32ad7ddf59cf204ade06d8
Sławek Zdunek <slawekzdunek2.3.6@gmail.com>
2022-09-22 20:16:57 +0200

    Changed Files structure

--- KekEngine/Source.cpp
+++ KekEngine/Source.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include "Byte.h"
-#include "Log.h"
+
 int main()
 {
-	Log(Byte((0x00001111)));
+
 }
---
+++ KekEngine/src/KekEngine/Core/Console.h
@@ -0,0 +1,104 @@
+#pragma once
+#include <map>
+#include <string_view>
+#include <fstream>
+#include "Console.h"
+#include "KekEngine/Maths/Color.h"
+
+class ColorTable
+{
+	public:
+	bool isValidName(const std::string& str);
+	colRGB8i operator[](const std::string& str);
+	colRGB8i operator[](int ind);
+};
+class IConsole
+{
+	public:
+
+	static ColorTable colorTable;
+
+	/// <summary>
+	///White, Black, Blue, Green, Cyan, Red, Magenta, Yellow, Gray, LGray, LBlue, LGreen, LCyan, LRed, LMagenta, LYellow
+	/// </summary>
+	/// <param name="colorName"></param>
+	void SetColor(const std::string& colorName);
+	/// <summary>
+	///White, Black, Blue, Green, Cyan, Red, Magenta, Yellow, Gray, LGray, LBlue, LGreen, LCyan, LRed, LMagenta, LYellow
+	/// </summary>
+	/// <param name="colorName"></param>
+	void SetBackColor(const std::string& colorName);
+
+	virtual void SetIndention(int indention) = 0;
+	virtual void SetColor(const colRGB8i& col) = 0;
+	virtual void SetBackColor(const colRGB8i& col) = 0;
+	virtual void SetBold(bool state) = 0;
+	virtual void SetUnderline(bool state) = 0;
+	virtual void SetDefault() = 0;
+
+	virtual int GetIndention() = 0;
+	virtual colRGB8i GetColor() = 0;
+	virtual colRGB8i GetBackColor() = 0;
+	virtual bool GetBold() = 0;
+	virtual bool GetUnderline() = 0;
+
+	virtual char GetLastLoggedChar() = 0;
+	virtual std::ostream& operator << (std::string_view sv) = 0;
+	virtual std::ostream& operator << (char sv) = 0;
+};
+
+class WindowsConsole : public IConsole
+{
+	colRGB8i fontCol = colRGB8i(192, 192, 192);
+	colRGB8i backCol = colRGB8i(0, 0, 0);
+	bool isBold = false;
+	bool isUnderline = false;
+	unsigned char indentionLevel = 0;
+	char lastLoggedChar = '\n';
+
+	public:
+	WindowsConsole() {}
+
+	void SetIndention(int indention);
+	void SetColor(const colRGB8i& col);
+	void SetBackColor(const colRGB8i& col);
+	void SetBold(bool state);
+	void SetUnderline(bool state);
+	void SetDefault();
+
+	int GetIndention();
+	colRGB8i GetColor();
+	colRGB8i GetBackColor();
+	bool GetBold();
+	bool GetUnderline();
+
+	char GetLastLoggedChar();
+	std::ostream& operator << (std::string_view sv);
+	std::ostream& operator << (char sv);
+};
+class FileConsole : public IConsole
+{
+	std::ofstream ofstream;
+	unsigned char indentionLevel = 0;
+	char lastLoggedChar = '\n';
+
+	public:
+	FileConsole(const char* sv);
+
+	void SetIndention(int indention);
+	void SetColor(const colRGB8i& col);
+	void SetBackColor(const colRGB8i& col);
+	void SetBold(bool state);
+	void SetUnderline(bool state);
+	void SetDefault();
+
+	int GetIndention();
+	colRGB8i GetColor();
+	colRGB8i GetBackColor();
+	bool GetBold();
+	bool GetUnderline();
+
+	char GetLastLoggedChar();
+	std::ostream& operator << (std::string_view sv);
+	std::ostream& operator << (char sv);
+};
