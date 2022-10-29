c97b3e9649702b717eb866fb6d35e845c4c418c0
Sławek Zdunek <slawekzdunek2.3.6@gmail.com>
2022-10-29 14:28:40 +0200

    Big Commit

--- KekEngine/Source.cpp
+++ KekEngine/Source.cpp
@@ -1,29 +1,22 @@
 #include "KekEngine/Core/Log.h"
 #include "KekEngine/Core/Application.h"
 #include "KekEngine/SystemIO/SystemIO.h"
-#include <thread>
-#include <chrono>
-using namespace std::chrono_literals;
 /// KURDE JAK MIE PROGRAM ZWALNIA TO ZAS MI KLAWIATURA NIE DZIAA DOBRZE.
 /// ADD MODULATORS TO SYSTEM_IO TO MATCH GLFW, AND ALSO PUT THEM BACK TO GLFW.
 using namespace Kek;
 
-void LogMouse(vec2i mpos)
-{
-	Log<Info>(mpos);
-}
 Application app;
 void Kek::Setup()
 {
 	SystemIO::Init();
-	SystemIO::MouseMoveEvent() += LogMouse;
-	app.AddWindow(Window{"window", vec2i(800,600)});
 }
 void Kek::Update()
 {
-	if(app.WindowCount() == 0) app.Close();
-	for(int i = 0; i < app.WindowCount(); i++)
+	if(SystemIO::Key(Key_Enter) != State_Click) return;
+
+	for(int i = 0; i < 5000; i++)
 	{
-		app[i].SwapBuffers();
+		SystemIO::SetKey({ Mouse_Left, State_Click });
 	}
+	app.Close();
 }
